c9bba63a8c7be9efca870a4f7ca7916b3565182e
Cristian Sotomayor <cristiansotomayor1912@gmail.com>
2018-11-20 23:42:51 -0500

    removing atributes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,26 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'prueba-ng';
   users: string[] = ['cristian', 'lucho', 'mari', 'diego', 'monica'];
-  activated = true;
-  // vamos a ver typescript a partir de aqui
-  name: string = 'Cristian Sotomayor';
-  age: number = 19;
-  single: boolean = true; // :"v
-  address: {
-    street: string;
-    city: string;
-  };
-  // hobbies = ['nadar', 'leer', 'escribir']
-  hobbies: string[];
 
-  constructor() {
-    this.address = {
-      street: 'Las Lilas mzn B lote 5',
-      city: 'Lima'
-    };
-    this.hobbies = ['nadar', 'leer', 'escribir'];
+  deleteUser(user) {
+    for (let i = 0; this.users.length; i++ ) {
+      if (user == this.users[i]) {
+        this.users.splice(i, 1);
+      }
+    }
   }
 
 }
